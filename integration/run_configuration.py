62e7d17c1c75635e474fe626d1e0851a43157911
mchiusi <marco.chiusi@cern.ch>
2023-12-04 18:18:04 +0100

    add parameters

--- integration/run_configuration.py
+++ integration/run_configuration.py
@@ -6,11 +6,20 @@ import yaml
 with open('config.yaml', "r") as afile:
     params = yaml.safe_load(afile)
 
-enum = cppyy.gbl.l1thgcfirmware.Step
-params['stepLatency'] = [{int(vars(enum)[latency]) : d[latency]} for d in params['stepLatency'] for latency in d.keys()]
-print(params['stepLatency'])
+def define_map(params):
+    enum = cppyy.gbl.l1thgcfirmware.Step
+    map_custom = cppyy.gbl.std.map[enum, "unsigned int"]()
+    
+    for d in params['stepLatency']:
+      for latency in d.keys():
+        map_custom[int(vars(enum)[latency])] = d[latency]
 
-ClusterAlgoConfig = cppyy.gbl.l1thgcfirmware.ClusterAlgoConfig
-config = ClusterAlgoConfig(**params)
+    params['stepLatency'] = map_custom
 
-config.printConfiguration()
+if __name__ == '__main__':
+    define_map(params)
+    
+    ClusterAlgoConfig = cppyy.gbl.l1thgcfirmware.ClusterAlgoConfig
+    config = ClusterAlgoConfig(**params)
+    
+    config.printConfiguration()
